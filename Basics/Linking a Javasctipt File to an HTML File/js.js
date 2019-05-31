8be36da3eaf51d2b9dcd6c6ea376d406e419864f
Paul Cuenin <pcuenin@gmail.com>
2019-05-31 16:30:31 -0400

    Add all files

---
+++ Basics/Linking a Javasctipt File to an HTML File/js.js
@@ -0,0 +1,4 @@
+
+document.write("This is our javascript being written first before anything else");
+
+console.log("This is text that will be sent to the console to let us know our program is succesful");
